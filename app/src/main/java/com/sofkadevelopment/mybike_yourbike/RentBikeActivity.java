a9b8b4c69d6e8b8ccfe9ad351ce79814947b0879
Sofiia Charnota <sofiacharnota@gmail.com>
2019-05-05 23:02:45 +0300

    Changes in RentBikeActivity.

--- app/src/main/java/com/sofkadevelopment/mybike_yourbike/RentBikeActivity.java
+++ app/src/main/java/com/sofkadevelopment/mybike_yourbike/RentBikeActivity.java
@@ -50,6 +50,7 @@ public class RentBikeActivity extends BaseActivity implements
     private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("bikes/Bikes") ;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,13 +60,6 @@ public class RentBikeActivity extends BaseActivity implements
         // Buttons
         findViewById(R.id.BacktoChoiceButton).setOnClickListener(this);
         findViewById(R.id.RentTheBikeButton).setOnClickListener(this);
-
-
-        // [START initialize_auth]
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
-
-
 
 
         db.collection("bikes")
@@ -79,108 +73,48 @@ public class RentBikeActivity extends BaseActivity implements
                         }
 
                         List bikes = new ArrayList<>();
+                        List bikes_ids = new ArrayList<>();
                         for (QueryDocumentSnapshot doc : value) {
-                            bikes.add(doc.getId()+doc.getData());
+                            bikes.add(doc.getData());
+                            bikes_ids.add(doc.getId());
 //                        bikes.add(doc.get("Brand"));
 //                        bikes.add(doc.get("City"));
 //                        bikes.add(doc.get("Phone"));
 //                        bikes.add(doc.get("Price"));
-
-
                         }
-                        ArrayAdapter<String>adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_checked,bikes);
-                        adapter.notifyDataSetChanged();
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_checked,bikes);
+        adapter.notifyDataSetChanged();
 
-                    mlistofBikes.setAdapter(adapter);
-                        // mPrintBikeView.setText(bikes.toString());
-                        Log.d(TAG, "Current bikes in DB: " + bikes);
+        mlistofBikes.setAdapter(adapter);
+        // mPrintBikeView.setText(bikes.toString());
+        Log.d(TAG, "Current bikes in DB: " + bikes);
 
-                   }
-                });
+        }
+        });
     }
 
-
-    //    // [START on_start_check_user]
     @Override
     public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        //FirebaseUser currentUser = mAuth.getCurrentUser();
-
+            super.onStart();
     }
-    // [END on_start_check_user]
+
     private void RentTheBike() {
-        db.collection("bikes")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            List<String> AllBikesData = new ArrayList<>();
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d(TAG, document.getId() + " => " + document.getData());
-                                AllBikesData.add(document.getId()+ document.getData());
-                                mPrintBikeView.setText(AllBikesData.toString());
-                            }
-                        } else {
-                            Log.d(TAG, "Error getting documents: ", task.getException());
-                        }
-                    }
-                });
+            Toast.makeText(getApplicationContext(),
+            "You rented the bike",
+            Toast.LENGTH_SHORT).show();
+    }
 
-
-}
-//
-//    private void AddTheBike(String brand, String price, String city, String phone) {
-//        Log.d(TAG, "Add bike");
-//        if (!validateForm()) {
-//            return;
-//        }
-//
-//        showProgressDialog();
-//
-//        // Create a new user with a first and last name
-//        Map<String, Object> bike = new HashMap<>();
-//        bike.put("Brand", brand);
-//        bike.put("Price", price);
-//        bike.put("City", city);
-//        bike.put("Phone", phone);
-//
-//// Add a new document with a generated ID
-//        db.collection("bikes").document( "Bikes").set(bike)
-//                .addOnSuccessListener(new OnSuccessListener<Void>() {
-//                    @Override
-//                    public void onSuccess(Void avoid) {
-//                        Log.d(TAG, "DocumentSnapshot successfully written: " );
-//                        Toast.makeText(getApplicationContext(),
-//                                "You added the bike",
-//                                Toast.LENGTH_SHORT).show();
-//                    }
-//                })
-//                .addOnFailureListener(new OnFailureListener() {
-//                    @Override
-//                    public void onFailure(@NonNull Exception e) {
-//                        Log.w(TAG, "Error writing document", e);
-//                        Toast.makeText(getApplicationContext(),
-//                                "Smth bad happened You did NOT add the bike",
-//                                Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//        hideProgressDialog();
-//
-//    }
 
     @Override
     public void onClick(View v) {
-        int i = v.getId();
-        if (i == R.id.BacktoChoiceButton) {
-            startActivity(new Intent(this, EmailPasswordActivity.class));
-            finish();
-
-        }
-        else if (i == R.id.RentTheBikeButton) {
-            RentTheBike();
-        }
+            int i = v.getId();
+            if (i == R.id.BacktoChoiceButton) {
+                startActivity(new Intent(this, EmailPasswordActivity.class));
+                finish();
+            }
+            else if (i == R.id.RentTheBikeButton) {
+                RentTheBike();
+            }
     }
 }
 
